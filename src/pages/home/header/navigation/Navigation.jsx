61f4415a454904818592c8d7c57cd88f952e180e
boyra <b9892213@gmail.com>
2025-04-01 16:42:35 +0300

    finished the nav bar

---
+++ src/pages/home/header/navigation/Navigation.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import Button from "../../../../components/button/Button";
+import classes from "./navigation.module.css";
+
+function Navigation() {
+  return (
+    <nav>
+      <ul className={classes["navigation"]}>
+        <li>
+          <Link to="/" title="home page">
+            Home
+          </Link>
+        </li>
+        <li>
+          <a href="#about" title="about me">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#projects" title="projects section">
+            Projects
+          </a>
+        </li>
+        <li>
+          <Button color={"black"}>Contacts</Button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export { Navigation as default };
