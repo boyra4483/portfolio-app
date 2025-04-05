2c88d5ec1d175957c8712dd7e44209f384084163
boyra <b9892213@gmail.com>
2025-04-05 13:50:20 +0300

    finished developing the burgerMenu functionality

--- src/pages/home/header/burger-menu/BurgerMenu.jsx
+++ src/pages/home/header/burger-menu/BurgerMenu.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import classes from "./burgerMenu.module.css";
 import Navigation from "../navigation/Navigation";
 
 function BurgerMenu() {
+  const [status, setStatus] = useState("");
+
+  function handleClick(e) {
+    const elem = e.target;
+    const parentElem = `nav[class~="${classes["nav-mobile"]}"]`;
+
+    if (!elem.closest(parentElem)) return;
+
+    status == "active" ? setStatus("") : setStatus("active");
+  }
+
   return (
-    <nav className={classes["nav-mobile"]}>
-      <section className={classes["burgerMenu"]}>
+    <nav
+      className={`${classes["nav-mobile"]} ${
+        status ? classes[status] : status
+      }`}
+    >
+      <section onClick={handleClick} className={classes["burgerMenu"]}>
         <div className={classes["line"]}></div>
       </section>
       <Navigation />
