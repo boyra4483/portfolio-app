326ee42ad308cb209d7469c425a52f1228df361f
boyra <b9892213@gmail.com>
2025-04-08 13:51:38 +0300

    finished the hero section of home page

---
+++ src/pages/home/hero/Hero.jsx
@@ -0,0 +1,24 @@
+import Button from "../../../components/button/Button";
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <article className={classes["hero"]}>
+      <header>
+        <h1 className={classes["name"]}>BOYRA</h1>
+      </header>
+      <p className={classes["about-me"]}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum
+        urna quis magna lobortis, eget finibus urna vestibulum. Sed nec ex non
+        justo dictum venenatis. Fusce nec purus nec mauris posuere malesuada
+        (About me in short)
+      </p>
+      <div className={classes["nav-buttons"]}>
+        <Button color={"white"}>Projects</Button>
+        <Button color={"black"}>Contacts</Button>
+      </div>
+    </article>
+  );
+}
+
+export { Hero as default };
