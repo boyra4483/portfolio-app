50ba5af61fa7b01ec5733eefa6d471d0592c4991
boyra <b9892213@gmail.com>
2025-04-12 15:34:36 +0300

    now it receives the onClick hundler

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,10 @@
 import classes from "./button.module.css";
 
-function Button({ color, children }) {
+function Button({ onClick, color, children }) {
   const BtnClass = color == "black" ? "blackBtn" : "whiteBtn";
   return (
     <a
+      onClick={onClick}
       href="#contacts"
       title="contacts"
       className={`${classes["button"]} ${classes[BtnClass]}`}
