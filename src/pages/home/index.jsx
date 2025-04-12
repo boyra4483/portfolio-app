59d8c6f2b083d999bd0296c531face1bf892f863
boyra <b9892213@gmail.com>
2025-04-12 15:28:59 +0300

    using the ref context

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,4 @@
+import { SectionsRefProvider } from "../../contexts/ref-context/RefContext";
 import Header from "./header/Header";
 import Hero from "./hero/Hero";
 import AboutMe from "./about-me/AboutMe";
@@ -5,13 +6,13 @@ import classes from "./index.module.css";
 
 function Home() {
   return (
-    <>
+    <SectionsRefProvider>
       <Header />
       <main className={classes["main"]}>
         <Hero />
         <AboutMe />
       </main>
-    </>
+    </SectionsRefProvider>
   );
 }
 
