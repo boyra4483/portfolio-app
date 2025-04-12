1ca1f73db6bb682c91c37b3cb7a53eae0644b639
boyra <b9892213@gmail.com>
2025-04-12 15:29:51 +0300

    added all nav-buttons except one the click event and attached handleClick hundler to them

--- src/pages/home/header/navigation/Navigation.jsx
+++ src/pages/home/header/navigation/Navigation.jsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
+import { useSectionsRef } from "../../../../contexts/ref-context/RefContext";
 import Button from "../../../../components/button/Button";
 import classes from "./navigation.module.css";
 
 function Navigation() {
+  const sectionsRef = useSectionsRef();
+
+  function handleClick(e) {
+    const section = sectionsRef.current.get(e.target.title);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
   return (
     <ul className={classes["navigation"]}>
       <li>
@@ -11,17 +22,19 @@ function Navigation() {
         </Link>
       </li>
       <li>
-        <a href="#about" title="about me">
+        <a onClick={handleClick} href="#about-me" title="about me">
           About
         </a>
       </li>
       <li>
-        <a href="#projects" title="projects section">
+        <a onClick={handleClick} href="#projects" title="projects section">
           Projects
         </a>
       </li>
       <li>
-        <Button color={"black"}>Contacts</Button>
+        <Button onClick={handleClick} color={"black"}>
+          Contacts
+        </Button>
       </li>
     </ul>
   );
