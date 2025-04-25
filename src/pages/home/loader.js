4eb1e93d9c4aace7160824282981a3f6857146f6
boyra <b9892213@gmail.com>
2025-04-25 14:17:05 +0300

    now project data with images is here

---
+++ src/pages/home/loader.js
@@ -0,0 +1,153 @@
+import wordleImg from "../../assets/pet-project-images/wordle.jpg";
+import todoImg from "../../assets/pet-project-images/to-do.jpg";
+import calculatorImg from "../../assets/pet-project-images/calculator.jpg";
+import weatherImg from "../../assets/pet-project-images/weather.jpg";
+import memoryCardGameImg from "../../assets/pet-project-images/memory-card-game.jpg";
+
+function homeLoader() {
+  return {
+    all: [
+      {
+        name: "memory-card-game",
+        techStack: "HTML CSS JS firebase",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: memoryCardGameImg,
+        links: [
+          "https://github.com/boyra4483/memory-card-game",
+          "https://boyra4483.github.io/memory-card-game/",
+        ],
+        id: 0,
+      },
+      {
+        name: "wordle",
+        techStack: "HTML CSS JSX REACT AXIOS",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: wordleImg,
+        links: [
+          "https://github.com/boyra4483/wordle",
+          "https://boyra4483.github.io/wordle/",
+        ],
+        id: 1,
+      },
+      {
+        name: "to-do",
+        techStack: "HTML CSS JSX REACT",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: todoImg,
+        links: [
+          "https://github.com/boyra4483/to-do-app",
+          "https://boyra4483.github.io/to-do-app/",
+        ],
+        id: 2,
+      },
+      {
+        name: "weather-app",
+        techStack: "HTML CSS JSX REACT REACT-ROUTER",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: weatherImg,
+        links: [
+          "https://github.com/boyra4483/weather-app",
+          "https://boyra4483.github.io/weather-app/",
+        ],
+        id: 3,
+      },
+      {
+        name: "calculator",
+        techStack: "HTML CSS JSX  REACT",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: calculatorImg,
+        links: [
+          "https://github.com/boyra4483/calculator-app",
+          "https://boyra4483.github.io/calculator-app/",
+        ],
+        id: 4,
+      },
+    ],
+    "html css": [],
+    javascript: [
+      {
+        name: "memory-card-game",
+        techStack: "HTML CSS JS firebase",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: memoryCardGameImg,
+        links: [
+          "https://github.com/boyra4483/memory-card-game",
+          "https://boyra4483.github.io/memory-card-game/",
+        ],
+        id: 0,
+      },
+    ],
+    reactJs: [
+      {
+        name: "wordle",
+        techStack: "HTML CSS JSX REACT AXIOS",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: wordleImg,
+        links: [
+          "https://github.com/boyra4483/wordle",
+          "https://boyra4483.github.io/wordle/",
+        ],
+        id: 1,
+      },
+      {
+        name: "to-do",
+        techStack: "HTML CSS JSX REACT",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: todoImg,
+        links: [
+          "https://github.com/boyra4483/to-do-app",
+          "https://boyra4483.github.io/to-do-app/",
+        ],
+        id: 2,
+      },
+      {
+        name: "weather-app",
+        techStack: "HTML CSS JSX REACT REACT-ROUTER",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: weatherImg,
+        links: [
+          "https://github.com/boyra4483/weather-app",
+          "https://boyra4483.github.io/weather-app/",
+        ],
+        id: 3,
+      },
+      {
+        name: "calculator",
+        techStack: "HTML CSS JSX  REACT",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: calculatorImg,
+        links: [
+          "https://github.com/boyra4483/calculator-app",
+          "https://boyra4483.github.io/calculator-app/",
+        ],
+        id: 4,
+      },
+    ],
+    "reactJs react-router": [
+      {
+        name: "weather-app",
+        techStack: "HTML CSS JSX REACT REACT-ROUTER",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolores eveniet incidunt temporibus maiores. Saepe nobis, possimus animi ad, eum, nemo nam voluptatem explicabo distinctio mollitia quas alias soluta impedit!",
+        img: weatherImg,
+        links: [
+          "https://github.com/boyra4483/weather-app",
+          "https://boyra4483.github.io/weather-app/",
+        ],
+        id: 3,
+      },
+    ],
+  };
+}
+
+export { homeLoader as default };
