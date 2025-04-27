c2162b7fa231f9baaeae51acb6a90e1839d7e362
boyra <b9892213@gmail.com>
2025-04-27 14:06:05 +0300

    added new variable for defining 'button' jsx tag if it is form button rather than 'link' button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,10 +3,9 @@ import classes from "./button.module.css";
 
 function Button({ color, children }) {
   const articlesRef = useArticlesRef();
-
   const BtnClass = color == "black" ? "blackBtn" : "whiteBtn";
-  const toSection = children == "Projects" ? "projects" : "contacts";
 
+  const toSection = children == "Projects" ? "projects" : "contacts";
   function handleClick(e) {
     const article = articlesRef.current.get(e.target.title);
     article.scrollIntoView({
@@ -15,7 +14,24 @@ function Button({ color, children }) {
     });
   }
 
-  return (
+  let submitBtn;
+  if (children == "Submit") {
+    submitBtn = (
+      <button
+        style={{
+          cursor: "pointer",
+        }}
+        type="submit"
+        className={`${classes["button"]} ${classes[BtnClass]}`}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  return submitBtn ? (
+    submitBtn
+  ) : (
     <a
       onClick={handleClick}
       href={`#${toSection}`}
