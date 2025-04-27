2916da048c9da047acefdc0ea93ea4ef9063fd1b
boyra <b9892213@gmail.com>
2025-04-27 14:15:59 +0300

    imported the last section of page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,6 +3,7 @@ import Header from "./header/Header";
 import Hero from "./hero/Hero";
 import AboutMe from "./about-me/AboutMe";
 import Projects from "./projects/Projects";
+import Contacts from "./contacts/Contacts";
 import classes from "./index.module.css";
 
 function Home() {
@@ -13,6 +14,7 @@ function Home() {
         <Hero />
         <AboutMe />
         <Projects />
+        <Contacts />
       </main>
     </ArticlesRefProvider>
   );
