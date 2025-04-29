882c79b6c444bc80ddd1acbee09e2d5c1729add1
boyra <b9892213@gmail.com>
2025-04-29 19:52:10 +0300

    changed the root path in accordance with base URL

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/portfolio-app/",
     element: <Home />,
     loader: homeLoader,
     action: homeAction,
