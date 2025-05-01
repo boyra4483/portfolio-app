539cc39909fdaad12e76b40aad202cbcf3dfeb23
boyra <b9892213@gmail.com>
2025-05-01 13:44:41 +0300

    refactoring now i use reducer instead of "useState" hook

--- src/pages/home/header/burger-menu/BurgerMenu.jsx
+++ src/pages/home/header/burger-menu/BurgerMenu.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import classes from "./burgerMenu.module.css";
 import Navigation from "../navigation/Navigation";
+import statusReducer from "../status-reducer/statusReducer";
 
 function BurgerMenu() {
-  const [status, setStatus] = useState("");
+  const [status, dispatch] = useReducer(statusReducer, "");
 
   function handleClick(e) {
     const elem = e.target;
     const parentElem = `nav[class~="${classes["nav-mobile"]}"]`;
-
     if (!elem.closest(parentElem)) return;
-    status == "active" ? setStatus("") : setStatus("active");
+
+    dispatch({
+      type: status == "" ? "opened-menu" : "closed menu",
+    });
   }
 
   return (
